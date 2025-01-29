4098c10806d0aa865f4b3a0f6dee2f7114e2de14
Mahdi khodabakhshi <mahdii.khodabakhshi@gmail.com>
2025-01-29 14:51:58 +0100

    Implement use mealOrder class in main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import org.example.constance.Size;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        MealOrder regularMeal = new MealOrder();
+        regularMeal.addBurgerTopping("BACON", "CHEESE", "MAYO");
+        regularMeal.setDrinkSize(Size.LARGE);
+        regularMeal.printItemizedList();
+
+        MealOrder secondMeal = new MealOrder("turkey", "sprite", "chili");
+        secondMeal.addBurgerTopping("LETTUCE", "CHEESE", "MAYO");
+        secondMeal.setDrinkSize(Size.SMALL);
+        secondMeal.printItemizedList();
+
+        MealOrder deluxeMeal = new MealOrder("deluxe", "Pepsi", "fries");
+        deluxeMeal.addBurgerTopping("AVOCADO", "BACON", "LETTUCE", "CHEESE", "MAYO");
+        deluxeMeal.setDrinkSize(Size.SMALL);
+        deluxeMeal.printItemizedList();
     }
 }
